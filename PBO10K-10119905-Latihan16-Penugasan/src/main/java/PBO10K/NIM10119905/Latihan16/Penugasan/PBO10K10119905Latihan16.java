5cb19f836c8880d1a5f0994bdfb28bfd72e42028
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:20:17 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan16-Penugasan/src/main/java/PBO10K/NIM10119905/Latihan16/Penugasan/PBO10K10119905Latihan16.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan16.Penugasan;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program menampilkan Penugasan
+ */
+public class PBO10K10119905Latihan16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c+"\n");
+        
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= 2;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+    }
+    
+}
